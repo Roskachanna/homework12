b330c4c809eaf04c2efc5ceadd0b35b13dd02ac6
annaroskach <danuta_tyt@mail.ru>
2023-02-08 19:06:19 +0300

    homework 12

--- src/MetodsAndClass/Book.java
+++ src/MetodsAndClass/Book.java
@@ -1,17 +1,21 @@
 package MetodsAndClass;
 
+import java.util.Objects;
+
 public class Book {
 
     private String bookName;
     private int publishingYear;
-    private Autors authorName;
+    private Author authorName;
 
 
-    public Book(String bookName, int publishingYear, Autors authorName) {
+    public Book(String bookName, int publishingYear, Author authorName) {
         this.bookName = bookName;
         this.publishingYear = publishingYear;
         this.authorName = authorName;
     }
+
+
     public void setPublishingYear(int publishingYear) {
         this.publishingYear = publishingYear;
     }
@@ -21,7 +25,32 @@ public class Book {
     public String getName() {
         return this.bookName;
     }
-    public Autors getAuthor() {
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", publishingYear=" + publishingYear +
+                ", authorName=" + authorName +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publishingYear == book.publishingYear && Objects.equals(bookName, book.bookName) && Objects.equals(authorName, book.authorName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, publishingYear, authorName);
+    }
+
+    public Author getAuthor() {
+
         return this.authorName;
     }
+
 }
